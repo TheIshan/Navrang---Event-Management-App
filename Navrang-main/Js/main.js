f891e32be39b3b6c2a688a9279a1e25831bdc076
TheIshan <133317507+TheIshan@users.noreply.github.com>
2025-05-09 16:54:20 +0530

    Add files via upload

--- Navrang-main/Js/main.js
+++ Navrang-main/Js/main.js
@@ -1,1 +1,9 @@
 
+// Fixing BS Scroll Navbar Glitch 
+document.addEventListener("DOMContentLoaded", function () {
+    var myOffcanvas = document.getElementById('offcanvasExample');
+    var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+    document.getElementById("btn").addEventListener('click', function (e) {
+        bsOffcanvas.toggle();
+    });
+});
