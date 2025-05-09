2a456e636e0653f7622e4aa4cbc88d5d6a727634
TheIshan <133317507+TheIshan@users.noreply.github.com>
2025-05-09 16:44:35 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-// Fixing BS Scroll Navbar Glitch 
-document.addEventListener("DOMContentLoaded", function () {
-    var myOffcanvas = document.getElementById('offcanvasExample');
-    var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
-    document.getElementById("btn").addEventListener('click', function (e) {
-        bsOffcanvas.toggle();
-    });
-});
